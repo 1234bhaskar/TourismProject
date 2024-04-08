c2fe47f682c2ce2514cadb99ff2fcb9b69e4c737
1234bhaskar <bhaskar2021chand@gmail.com>
2024-04-08 17:38:11 +0530

    Created App  with Routes of Different pages

--- TourismProject/src/App.jsx
+++ TourismProject/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import Home from './Pages/Home'
+import Booking from './Pages/Booking'
+import Information from './Pages/Information'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div className=''>
+    <Router>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/booking' element={<Booking/>}/>
+      <Route path='/info' element={<Information/>}/>
+      
+    </Routes>
+    </Router>
+    </div>
   )
 }
